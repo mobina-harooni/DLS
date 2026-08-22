e4826e72930bfeb70c6c60036e6ae9378a229b98
mobina-harooni <mobinaharooni@gmail.com>
2026-08-22 17:38:16 +0330

    Refactor EBook class and complete open method

--- Ebook.py
+++ Ebook.py
@@ -1,8 +1,9 @@
 from book import Book
 class Ebook(Book):
    def __init__(self,title:str,author:str,isbn:str,year:int,genre:str,file_size:float,format:str):
-      super().__init__(title,author,isbn,year,genre)
-      self.file_size = file_size
-      self.format = format
+        super().__init__(title,author,isbn,year,genre)
+        self.file_size = file_size
+        self.format = format
 
-    
+   def open(self):
+       print(f"opening Book {self.title} , {self.format},{self.file_size}")
